7b16a7ff4b80b72f5e6e0fd2e0eac61d363e0f96
dumisa-sakhile <dumisa.developer@gmail.com>
2024-06-12 23:46:16 +0200

    add movie/tv page toggle

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 import './style.css'
 import { $, $$, none, flex, grid, block } from './utilities.js'
 
+const moviesOpen = $("#movies-open");
+const tvOpen = $("#tv-open");
+const movies = $("#movies");
+const tv = $("#tv");
+
+
+
+tvOpen.addEventListener("click", () => {
+  none(movies);
+  flex(tv);
+  tvOpen.classList="btn-active";
+  moviesOpen.classList="btn-disabled";
+});
+
+moviesOpen.addEventListener("click", () => {
+  none(tv);
+  flex(movies);
+  moviesOpen.classList = "btn-active";
+  tvOpen.classList = "btn-disabled";
+});
--- utilities.js
+++ utilities.js
@@ -5,17 +5,17 @@ export function $$(selector) {
   return document.querySelectorAll(selector);
 }
 export function none(selector) {
-  $(selector).classList.add("hidden");
+  selector.classList.add("hidden");
 }
 export function flex(selector) {
-  $(selector).classList.remove("hidden");
-  $(selector).classList.add("flex");
+  selector.classList.remove("hidden");
+  selector.classList.add("flex");
 }
 export function grid(selector) {
-  $(selector).classList.remove("hidden");
-  $(selector).classList.grid("flex");
+  selector.classList.remove("hidden");
+  selector.classList.grid("flex");
 }
 export function block(selector) {
-  $(selector).classList.remove("hidden");
-  $(selector).classList.block("flex");
+  selector.classList.remove("hidden");
+  selector.classList.block("flex");
 }
