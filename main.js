6055827aa3f34a4ea72cb3525fe4ea30f70c9e38
dumisa-sakhile <dumisa.developer@gmail.com>
2024-06-13 03:38:04 +0200

    Change layout and add alerts

--- main.js
+++ main.js
@@ -1,23 +1,56 @@
 import './style.css'
-import { $, $$, none, flex, grid, block } from './utilities.js'
+import { $, $$, none, flex, grid, block,alert } from './utilities.js';
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+
 
 const moviesOpen = $("#movies-open");
 const tvOpen = $("#tv-open");
 const movies = $("#movies");
 const tv = $("#tv");
-
+const movieFilter = $("#movieFilter");
 
 
 tvOpen.addEventListener("click", () => {
   none(movies);
   flex(tv);
-  tvOpen.classList="btn-active";
-  moviesOpen.classList="btn-disabled";
+
+  alert("TV Shows", "Welcome to the TV Shows page!",200)
+
 });
 
 moviesOpen.addEventListener("click", () => {
   none(tv);
   flex(movies);
-  moviesOpen.classList = "btn-active";
-  tvOpen.classList = "btn-disabled";
+
+  alert("Movies", "Welcome to the Movies page!", 200);
+
 });
+
+
+tippy("#movie-filter", {
+  content: "Open movie filters!",
+});
+
+tippy("#tv-filter", {
+  content: "Open tv filters!",
+});
+
+
+tippy("#tv-open", {
+  content: "Switch to TV Shows!",
+});
+
+tippy("#movie-open", {
+  content: "Switch to the Movies!",
+});
+
+tippy("#close-alert", {
+  content: "Close!",
+});
+
+$("#close-alert").addEventListener("click", ()=>{
+  none($("#alert"));
+})
+
+alert("movie not found","requested movie could not be found",404)
--- utilities.js
+++ utilities.js
@@ -19,3 +19,18 @@ export function block(selector) {
   selector.classList.remove("hidden");
   selector.classList.block("flex");
 }
+
+export function alert(alertTitle,alertMessage,alertType){
+flex($("#alert"));
+$("#alert-title").textContent = alertTitle;
+$("#alert-message").textContent = alertMessage;
+
+if (alertType == 404) {
+  $("#alert").classList.remove("bg-indigo-500");
+  $("#alert").classList.add("bg-red-500");
+} else if (alertType == 200) {
+  $("#alert").classList.remove("bg-red-500");
+  $("#alert").classList.add("bg-indigo-500");
+}
+
+}
