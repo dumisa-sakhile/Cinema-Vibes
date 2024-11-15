b69b1872f7b18ea8838ac272cf36f3437efa37dd
Sakhile Dumisa <contact.dumisa@gmail.com>
2024-11-15 05:09:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
-import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig({
-  plugins: [EnvironmentPlugin(["TMDB_API_KEY"])],
+  build: {
+    sourcemap: false,
+  },
+  envDir: './',
+  envPrefix: 'TMDB_API_KEY',
 });
